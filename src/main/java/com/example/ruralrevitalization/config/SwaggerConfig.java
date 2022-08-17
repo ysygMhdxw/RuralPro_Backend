d0cc75b5f9fea1cfaad1e5efc1bcebabd2976473
ysygMhdxw <75829561+ysygMhdxw@users.noreply.github.com>
2022-08-17 18:46:59 +0800

    resolve swaggerVersion

--- src/main/java/com/example/ruralrevitalization/config/SwaggerConfig.java
+++ src/main/java/com/example/ruralrevitalization/config/SwaggerConfig.java
@@ -9,21 +9,22 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    //配置Swagger2的Docket的定实例
+    // 配置Swagger2的Docket的定实例
     @Bean
-    public Docket docket(){
-        return  new Docket(DocumentationType.SWAGGER_2)
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
                 // 如果关闭swagger服务
                 // .enable(false)
                 .apiInfo(apiInfo());
     }
 
-    //配置Swagger信息 apiInfo类
-    private ApiInfo apiInfo(){
-        //作者信息
+    // 配置Swagger信息 apiInfo类
+    private ApiInfo apiInfo() {
+        // 作者信息
         Contact DEFAULT_CONTACT = new Contact("心心”乡“惜", "https://github.com/ysygMhdxw/RuralPro_Backend", "");
 
         return new ApiInfo("RuralPro API文档",
@@ -36,4 +37,4 @@ public class SwaggerConfig {
                 new ArrayList<>());
     }
 }
-//测试运行：http://localhost:9654/swagger-ui.html
+// 测试运行：http://localhost:9654/swagger-ui.html
